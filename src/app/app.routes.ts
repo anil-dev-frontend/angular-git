996153d45970cc3f09432423e9ba1513abf8d9ad
Anil Yadav <133839287+AnilKYad@users.noreply.github.com>
2025-12-31 15:47:07 +0530

    Home component add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HOME_ROUTES } from './features/home/homes.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...HOME_ROUTES
+];
